e94c5141774872987286e26d4e729f0ba0a42c1d
Itsmeelo <elenakuparadze@gmail.com>
2024-08-25 13:14:31 +0400

    operation with coins

--- coke.js
+++ coke.js
@@ -1,4 +1,18 @@
 import { input, print } from './bitcamp/lib.js';
 
-let change = input("Please toss your coin here 👇");
+let coin = Number(input("Please toss your coin here 👉 "));
+
+
+let b;
+
+    if (coin < 50 && coin == 5 || coin == 10 || coin == 25){
+        b = 50 - coin;
+        print('Amount Duo:', b);
+    } else {
+        print('Amount Duo: 50');
+    }
+
+    
+
+
 
