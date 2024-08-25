86afbf899025f9b273ad42e256d715b007a2fb2c
Itsmeelo <elenakuparadze@gmail.com>
2024-08-25 13:58:11 +0400

    program content change

--- coke.js
+++ coke.js
@@ -4,13 +4,26 @@ let coin = Number(input("Please toss your coin here 👉 "));
 
 
 let b;
+let coin2; 
+
 
     if (coin < 50 && coin == 5 || coin == 10 || coin == 25){
         b = 50 - coin;
         print('Amount Duo:', b);
+        while (coin < 50) {
+            coin2 = number(input("Please toss your coin here 👉 "));
+            coin = coin + coin2;
+        }
+        if (coin == 50) {
+            print ('Change Owed: 0')
+        } else if (coin > 50){
+            print ('Change Owed:', coin - 50);
+        }
     } else {
-        print('Amount Duo: 50');
+        print ('Amount Duo: 50');
+        // coin = Number(input("Please toss your coin here 👉 "));
     }
+    
 
     
 
